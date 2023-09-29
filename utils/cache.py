acc7f19d8260b30cb785bc881f7eafb724b31aac
SeanOzzy <>
2023-09-29 19:54:55 +1000

    Release version 1.1 with improved per region cache expiry

--- utils/cache.py
+++ utils/cache.py
@@ -17,15 +17,25 @@ CACHE_VALIDITY_DAYS = constants.cache_expiry_days
 # If found it returns the data from the cache file
 def get_from_cache(cache_key):
     cache_file = os.path.join(CACHE_DIR, f"{cache_key}.json")
-    
+
     if os.path.exists(cache_file):
         file_mtime = datetime.fromtimestamp(os.path.getmtime(cache_file))
         if datetime.now() - file_mtime < timedelta(days=CACHE_VALIDITY_DAYS):
             try:
                 with open(cache_file, 'r') as file:
-                    data = json.load(file)
+                    all_data = json.load(file)
                     region = os.environ.get('RDS_REGION', 'default_region')
-                    return data.get(region, None)
+                    region_data = all_data.get(region, None)
+
+                if region_data:
+                    cache_timestamp = datetime.fromisoformat(region_data["timestamp"])
+                    if datetime.now() - cache_timestamp < timedelta(days=CACHE_VALIDITY_DAYS):
+                        return region_data["data"]
+                    else:
+                        # Cache expired for the region
+                        del all_data[region]
+                        with open(cache_file, 'w') as file:
+                            json.dump(all_data, file)
             except Exception as e:
                 logger.error(f"Could not read from cache: {e}")
                 # print(f"Could not read from cache: {e}")
@@ -38,14 +48,17 @@ def save_to_cache(cache_key, data):
         os.makedirs(CACHE_DIR)
 
     cache_file = os.path.join(CACHE_DIR, f"{cache_key}.json")
-    
+
     current_data = {}
     if os.path.exists(cache_file):
         with open(cache_file, 'r') as file:
             current_data = json.load(file)
 
     region = os.environ.get('RDS_REGION', 'default_region')
-    current_data[region] = data
+    current_data[region] = {
+        "timestamp": datetime.now().isoformat(),
+        "data": data
+    }
 
     try:
         with open(cache_file, 'w') as file:
